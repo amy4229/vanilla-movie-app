18680cb92b47bc7645c6e7c79cc7579a99916e36
Amy <amykim4229@gmail.com>
2023-02-23 23:35:42 +0900

    #3 영화관련 상태 관리 기능
    영화 검색어,
    페이지로딩,
    영화검색결과
    최대 페이지 수
    로딩
    메세지
    
    메인화면구성에 필요한 모든 상태를 관리하는 기능을 작성하였다.

---
+++ src/stores/movie.js
@@ -0,0 +1,40 @@
+import Store from '../core/store'
+
+const store = new Store({
+  searchText: '',
+  page: 1,
+  movies: [],
+  maxPage: 1,
+  loading: false,
+  message: 'Search for the movie title'
+})
+
+export default store
+export const searchMovies = async page => {
+  store.state.loading = true;
+  store.state.page = page;
+  if (page === 1) {
+    store.state.movies = [];
+    store.state.message = '';
+  }
+
+  try {
+    const res = await fetch(`https://www.omdbapi.com?apikey=ecdd41da&s=${store.state.searchText}&page=${page}`);
+    const { Search, totalResults, Response, Error } = await res.json();
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies
+        , ...Search
+      ];
+
+      store.state.maxPage = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error
+    }
+  } catch (error) {
+    console.log(`searchMovies errors:`, error);
+  } finally {
+    store.state.loading = false;
+  }
+
+}
