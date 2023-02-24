be9208e84c983234d834dfc59005e897ab4e4777
Amy <amykim4229@gmail.com>
2023-02-24 13:54:38 +0900

    #5 html수정
    정보영역을 담는 spec 클래스 추가

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -6,39 +6,43 @@ export default class Detail extends Component {
     this.el.classList.add("movie-detail", "container");
     await getMovieDetails(history.state.id);
     const { Actors, Country, Director, Genre, Plot, Title, Poster, Production, Released, Runtime, Ratings } = movieStore.state.movie;
+    const resize_poster = Poster.replace("SX300", "SX500");
     this.el.innerHTML = /*html*/`
-      <div style="background-image:url('${Poster}')" class="poster"></div>
-      <div class="title"> ${Title}</div>
-      <div class="lables">
-        <span>${Released}</span>
-        &nbsp;/&nbsp;
-        <span>${Runtime}</span>
-        &nbsp;/&nbsp;
-        <span>${Country}</span>
+      <div style="background-image:url('${resize_poster}')" class="poster"></div>
+      <div className="specs">
+        <div class="title"> ${Title}</div>
+        <div class="lables">
+          <span>${Released}</span>
+          &nbsp;/&nbsp;
+          <span>${Runtime}</span>
+          &nbsp;/&nbsp;
+          <span>${Country}</span>
+        </div>
+        <div class="plot">
+          ${Plot}
+        </div>
+        <div>
+          <h3>Ratings</h3>
+          ${Ratings.map(rating => `<p>${rating.Source} : ${rating.Value}</p>`).join("")}
+        </div>
+        <div>
+          <h3>Actors</h3>
+          <p>${Actors}</p>
+        </div>
+        <div>
+          <h3>Director</h3>
+          <p>${Director}</p>
+        </div>
+        <div>
+          <h3>Production</h3>
+          <p>${Production}</p>
+        </div>
+        <div>
+          <h3>Genre</h3>
+          <p>${Genre}</p>
+        </div>
       </div>
-      <div class="plot">
-        ${Plot}
-      </div>
-      <div>
-        <h3>Ratings</h3>
-        ${Ratings.map(rating => `<p>${rating.Source} : ${rating.Value}</p>`).join("")}
-      </div>
-      <div>
-        <h3>Actors</h3>
-        <p>${Actors}</p>
-      </div>
-      <div>
-        <h3>Director</h3>
-        <p>${Director}</p>
-      </div>
-      <div>
-        <h3>Production</h3>
-        <p>${Production}</p>
-      </div>
-      <div>
-        <h3>Genre</h3>
-        <p>${Genre}</p>
-      </div>
+      
     `
 
   }
